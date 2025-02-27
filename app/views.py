7c0cfc9349f8e701e98992b39a81ffaedd22c98c
e13q <110967581+e13q@users.noreply.github.com>
2025-02-28 01:02:25 +0300

    add index view (cake making)

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
-from datetime import date, datetime, timedelta
+from django.shortcuts import render
 
+# from .forms import OrderForm
+import json
+from .models import ClientUser, Form, Level, Topping, Berry, Decor, Cake, Invoice, Order
 import phonenumbers as ph
 from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model, login, logout
@@ -14,7 +17,7 @@ from django.urls import reverse
 from django.views import View
 from phonenumbers import NumberParseException
 
-from .models import ClientUser
+from django.shortcuts import render
 
 
 def normalise_phone_number(pn):
@@ -28,7 +31,51 @@ def normalise_phone_number(pn):
 
 
 def index(request):
-    return render(request, "index.html")
+    levels_prices = list(Level.objects.values_list("price", flat=True))
+    levels_prices.insert(0, 0)
+    forms_prices = list(Form.objects.values_list("price", flat=True))
+    forms_prices.insert(0, 0)
+    toppings_prices = list(Topping.objects.values_list("price", flat=True))
+    toppings_prices.insert(0, 0)
+    berries_prices = list(Berry.objects.values_list("price", flat=True))
+    berries_prices.insert(0, 0)
+    decors_prices = list(Decor.objects.values_list("price", flat=True))
+    decors_prices.insert(0, 0)
+    costs = {
+        "Levels": levels_prices,
+        "Forms": forms_prices,
+        "Toppings": toppings_prices,
+        "Berries": berries_prices,
+        "Decors": decors_prices,
+        "Words": 500
+    }
+    levels_titles = list(Level.objects.values_list("title", flat=True))
+    forms_titles = list(Form.objects.values_list("title", flat=True))
+    toppings_titles = list(Topping.objects.values_list("title", flat=True))
+    berries_titles = list(Berry.objects.values_list("title", flat=True))
+    decors_titles = list(Decor.objects.values_list("title", flat=True))
+    context = {
+        "levels": levels_titles.copy(),
+        "forms": forms_titles.copy(),
+        "toppings": toppings_titles.copy(),
+        "berries": berries_titles.copy(),
+        "decors": decors_titles.copy(),
+        "costs_json": json.dumps(costs),
+    }
+    levels_titles.insert(0, 'не выбрано')
+    forms_titles.insert(0, 'не выбрано')
+    toppings_titles.insert(0, 'не выбрано')
+    berries_titles.insert(0, 'нет')
+    decors_titles.insert(0, 'нет')
+    data = {
+        "Levels": levels_titles,
+        "Forms": forms_titles,
+        "Toppings": toppings_titles,
+        "Berries": berries_titles,
+        "Decors": decors_titles,
+    }
+    context.update({"data_json": json.dumps(data)})
+    return render(request, "index.html", context)
 
 
 class ClientLoginView(View):
